dd02f6354edabdb5d0224d6b3cf7fe8a99f3b136
darya-startsava <daryastartsava@gmail.com>
2023-06-25 11:50:07 +0200

    feat: add invalid input message

--- index.js
+++ index.js
@@ -13,16 +13,71 @@ console.log(`You are currently in ${process.cwd()}`);
 rl.on('close', () => { console.log(`\n Thank you for using File Manager, ${userName}, goodbye! \n`); });
 
 rl.on('line', (input) => {
-  switch (input) {
-    case '.exit':
-      rl.close();
-      break;
-    default:
-      console.log('default');
-  }
-  if (input !== '.exit') {
+  const inputArray = input.split(' ');
+  if (inputArray.length === 1) {
+    switch (inputArray[0]) {
+      case '.exit':
+        rl.close();
+        break;
+      case 'up':
+        console.log('up');
+        break;
+      case 'ls':
+        console.log('ls');
+        break;
+      default:
+        console.log('Invalid input');
+    }
+    if (input !== '.exit') {
+      console.log(`\n You are currently in ${process.cwd()} \n`);
+    };
+  } else if (inputArray.length === 2) {
+    switch (inputArray[0]) {
+      case 'cd':
+        console.log('cd');
+        break;
+      case 'cat':
+        console.log('cat');
+        break;
+      case 'add':
+        console.log('add');
+        break;
+      case 'rm':
+        console.log('rm');
+        break;
+      case 'os':
+        console.log('os');
+        break;
+      case 'hash':
+        console.log('hash');
+        break;
+      default:
+        console.log('Invalid input');
+    }
     console.log(`\n You are currently in ${process.cwd()} \n`);
-  };
+  } else if (inputArray.length === 3) {
+    switch (inputArray[0]) {
+      case 'rn':
+        console.log('rn');
+        break;
+      case 'cp':
+        console.log('cp');
+        break;
+      case 'mv':
+        console.log('mv');
+        break;
+      case 'compress':
+        console.log('compress');
+        break;
+      case 'decompress':
+        console.log('decompress');
+        break;
+      default:
+        console.log('Invalid input');
+    }
+    console.log(`\n You are currently in ${process.cwd()} \n`);
+  }
+
 });
 
 
